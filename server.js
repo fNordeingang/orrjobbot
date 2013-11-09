51387e786c301c7aa70e240d29ab2fa7dc595dde
vileda <vileda@piratenfraktion-nrw.de>
2013-11-09 16:56:24 +0100

    fooo

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var app = require('express')()
 var twitter = require('ntwitter');
 var express = require('express');
 
-if ( process.argv.length < 6 ) {
-  console.log("usage: node server.js [consumer_key] [consumer_secret] [access_token_key] [access_token_secret]");
+if ( process.argv.length < 7 ) {
+  console.log("usage: node server.js [consumer_key] [consumer_secret] [access_token_key] [access_token_secret] [track]");
   process.exit(1);
 }
 
@@ -18,10 +18,18 @@ var twit = new twitter({
     access_token_secret : process.argv[5]
 });
 
+var track = process.argv[6];
+
 app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('connection', function (socket) {
-  twit.stream('statuses/filter', {'track': 'fdp' }, function(stream) {
+  twit.search(track.split(',').join(' OR '), {}, function(err, data) {
+    for(var tweet in data.statuses) {
+      socket.emit('tweet', { data: data.statuses[tweet] });
+    }
+    console.log(err);
+  });
+  twit.stream('statuses/filter', {'track': track }, function(stream) {
     //twit.stream('statuses/sample', function(stream) {
     stream.on('data', function (data) {
       socket.emit('tweet', { data: data });
